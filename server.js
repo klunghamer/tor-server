bd4aed051a0b7053c60e16357f0c63571ac45bcc
klunghamer <kellylunghamer@Kellys-MacBook-Pro.local>
2018-04-23 22:02:38 -0400

    working

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+
+app.use(express.static('public'));
+
+app.listen(8080, function() {
+    console.log('=======================');
+    console.log('Running on port ' + 8080);
+    console.log('=======================');
+});
